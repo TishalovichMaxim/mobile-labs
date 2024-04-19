96a6ef38d6d6e7fdf32e33ca795b9f37c98ce9a5
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-19 19:13:52 +0300

    Add sign up logic + date to string conversion to SignUpViewModel.

--- app/src/main/java/com/bsuir/castles/viewmodel/SignUpViewModel.kt
+++ app/src/main/java/com/bsuir/castles/viewmodel/SignUpViewModel.kt
@@ -1,9 +1,16 @@
 package com.bsuir.castles.viewmodel
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.bsuir.castles.viewmodel.help.FirestorePath
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
+import kotlinx.coroutines.tasks.await
+import java.text.SimpleDateFormat
 import java.util.Date
 
 class SignUpViewModel : ViewModel() {
@@ -24,7 +31,42 @@ class SignUpViewModel : ViewModel() {
 
     var showDatePicker by mutableStateOf(false)
 
+    @SuppressLint("SimpleDateFormat")
+    fun getDateString(): String {
+        val dateFormat = SimpleDateFormat("dd/mm/yyyy")
+        date?.let {
+            return dateFormat.format(it)
+        }
+
+        return "Choose.."
+    }
+
+    suspend fun signUp() {
+        val res = Firebase.auth
+            .createUserWithEmailAndPassword(email, password)
+            .await()
+
+        if (res.user == null) {
+            //bad auth
+            return
+        }
+
+        val user = {
+            "bio" to bio
+            "birthdate" to date
+            "firstname" to firstName
+            "lastname" to lastName
+        }
+
+        val storingRes = Firebase.firestore
+            .collection(FirestorePath.USERS.path)
+            .add(user)
+            .await()
+
+        goToSignInScreen()
+    }
+
     fun goToSignInScreen() {
-        Router.instance.route(Screen.SignIn);
+        Router.instance.route(Screen.SignIn)
     }
 }
