f6d08ca80ce05ed30a7f0d980c647fe848199481
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-21 10:50:54 +0300

    Add logout and date to string conversion to ProfileScreenViewModel.

--- app/src/main/java/com/bsuir/castles/viewmodel/ProfileScreenViewModel.kt
+++ app/src/main/java/com/bsuir/castles/viewmodel/ProfileScreenViewModel.kt
@@ -1,18 +1,30 @@
 package com.bsuir.castles.viewmodel
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
 import com.bsuir.castles.model.User
+import java.text.SimpleDateFormat
 
 class ProfileScreenViewModel : ViewModel() {
 
-    private var _user: User? = null
-
     var user: User
         get() {
-            return _user!!
+            return SharedData.user!!
         }
         set(value) {
-            _user = value
+            SharedData.user = value
         }
+
+    fun logout() {
+        SharedData.user = null
+
+        Router.instance.route(Screen.SignIn)
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    fun getDateString() : String {
+        val formatter = SimpleDateFormat("dd/MM/yyyy")
+        return formatter.format(user.birthdate)
+    }
 
 }
