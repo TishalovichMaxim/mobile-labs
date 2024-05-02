a4c8795b14b78f96ae9a8a151ab914802c043e75
Tishalovich Maxim <tishalovichm@gmail.com>
2024-05-02 22:41:13 +0300

    Add user and castle entities.

---
+++ castles/lib/model/castle.dart
@@ -0,0 +1,15 @@
+class Castle {
+
+  String id;
+
+  String name;
+
+  String country;
+
+  int foundationYear;
+
+  String mainImage;
+
+  Castle(this.id, this.name, this.country, this.foundationYear, this.mainImage);
+
+}
---
+++ castles/lib/model/user.dart
@@ -0,0 +1,15 @@
+class User {
+
+  String email;
+
+  String firstName;
+
+  String lastName;
+
+  String bio;
+
+  DateTime birthdate;
+
+  User(this.email, this.firstName, this.lastName, this.bio, this.birthdate);
+
+}
